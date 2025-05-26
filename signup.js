7efd463e807d7eff01aa6a2753941b6d4455a368
JstLevi <levibaron143@gmail.com>
2025-05-26 23:47:02 +0800

    slight change

--- signup.js
+++
@@ -1,9 +0,0 @@
-function handleSignup(event) {
-      event.preventDefault();
-
-      alert("Sign up successful!");
-
-      setTimeout(() => {
-        window.location.href = "customer_login.html";
-      }, 500);
-    }
